680ae13755b1fc1424cb9c52b45414e5f902a16f
Joan <joanpulle@gmail.com>
2021-02-04 00:25:52 +0100

    Eleccion de equipos acabada y pantalla inicio y primera lucha creada

--- js/main.js
+++ js/main.js
@@ -26,62 +26,98 @@ let player5 = new Luchador("Antimage", 300, 50,45,35);
 let player6 = new Luchador("Juggernaut",300, 45, 40,40);
 let player7 = new Luchador("Drow",300, 40, 45,40);
 let player8 = new Luchador("Viper",300, 50, 35,45);
+
+
 let p1="";
 let p2="";
 let p3="";
 let p4="";
+let p5="";
+let p6="";
 let equipoA = [];
 let equipoB = [];
 
+//Traductor de heroe seleccionado:
+let allheros = {
+    "Lich": player1,
+    "King": player2,
+    "Sniper": player3,
+    "Luna": player4,
+    "Antimage": player5,
+    "Juggernaut": player6,
+    "Drow": player7,
+    "Viper": player8,
+}
+//Cambiar de pantalla:
+let cambiaPantalla = (faseAhora,faseFutura) => {
+    let pantallaActual = document.getElementById(faseAhora);
 
+    let pantallaDestino = document.getElementById(faseFutura);
+
+    //aqui procedemos con el cambio
+
+    pantallaActual.style.display = "none";
+    pantallaDestino.style.display = "block";
+};
 //Seleccion de equipos
 let selectPersonaje = (heroe)=>{
 
         //Seleccion heroes equipo A
     if(equipoA.length < 3){
-        equipoA.push(heroe);
+        //equipoA.push(heroe);
+        equipoA.push(allheros[heroe]);
+    
+        
         document.getElementById(heroe).className="elegido";
         document.getElementById(heroe).onclick="";
+        
+
+
         rellenarRadiant();
+        
+        
         //Seleccion heroes equipo B
     }else if(equipoB.length < 3){
-        equipoB.push(heroe);
+        //equipoB.push(heroe);
+        equipoB.push(allheros[heroe]);
         document.getElementById(heroe).className="elegido";
         document.getElementById(heroe).onclick="";
-
-        
+   
+        //Cambiar de pantalla porque ya tenemos a los personajes elegidos
         rellenarDire();
+        resolveIn(1000).then(delay => {
 
-
-    };
+            cambiaPantalla("screen1","screen2");
+            
+        });
     
+    };
     console.log("El equipo A: ", equipoA);
     console.log("El equipo B: ", equipoB);
-    
-
 };
+
+//Función para el delay
+const resolveIn = delay =>
+new Promise(res => setTimeout(() => res(delay), delay));
+
 let rellenarRadiant = ()=>{
     for(let i=0; i < equipoA.length; i++ ){
         let showPlayer1 = document.getElementById("rival1");
-        showPlayer1.innerHTML = `<div ><img src="img/${equipoA[0]}.png"></div>`
+        showPlayer1.innerHTML = `<div ><img src="img/${equipoA[0].nombre}.png"></div>`
         let showPlayer2 = document.getElementById("rival2");
-        showPlayer2.innerHTML = `<div ><img src="img/${equipoA[1]}.png"></div>`
+        showPlayer2.innerHTML = `<div ><img src="img/${equipoA[1].nombre}.png"></div>`
         let showPlayer3 = document.getElementById("rival3");
-        showPlayer3.innerHTML = `<div ><img src="img/${equipoA[2]}.png"></div>`
-
+        showPlayer3.innerHTML = `<div ><img src="img/${equipoA[2].nombre}.png"></div>`
     };
 };
 let rellenarDire = ()=>{
     for(let i=0; i < equipoB.length; i++ ){
         let showPlayer4 = document.getElementById("rival4");
-        showPlayer4.innerHTML = `<div ><img src="img/${equipoB[0]}.png"></div>`
+        showPlayer4.innerHTML = `<div ><img src="img/${equipoB[0].nombre}.png"></div>`
         let showPlayer5 = document.getElementById("rival5");
-        showPlayer5.innerHTML = `<div ><img src="img/${equipoB[1]}.png"></div>`
+        showPlayer5.innerHTML = `<div ><img src="img/${equipoB[1].nombre}.png"></div>`
         let showPlayer6 = document.getElementById("rival6");
-        showPlayer6.innerHTML = `<div ><img src="img/${equipoB[2]}.png"></div>`
-
-        
-
+        showPlayer6.innerHTML = `<div ><img src="img/${equipoB[2].nombre}.png"></div>`
     };
 };
 
